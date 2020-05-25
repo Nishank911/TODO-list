c1f529688c93fac01d3fd11c223525a4f614dc83
NISHANK GUPTA <64626311+Nishank911@users.noreply.github.com>
2020-05-25 17:20:16 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php 
     include 'common.php';
-    
-    
-    
-    
+   
 ?>
 
 <!DOCTYPE html>
@@ -14,9 +11,28 @@ and open the template in the editor.
 -->
 <html>
     <head>
-         <!-- Latest compiled and minified CSS -->
+         
+        
+        
+        
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.21.0/moment.min.js" type="text/javascript"></script>
+        
+        
+        
+        
+        
+        
+        <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-       
+ 
+          
+<link href="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/build/css/bootstrap-datetimepicker.css" rel="stylesheet">
+<link rel="stylesheet" href="bootstrap-datetimepicker.min.css">
+<script src="bootstrap-datetimepicker.min.js"></script>
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
        <title>TODO</title>
@@ -36,22 +52,38 @@ and open the template in the editor.
         <div class="container">    
         <div class="row row-style">
             <div class="col-xs-offset-3 col-xs-6 ">
-        <form method="POST" action="task_display.php" >
+        <form method="POST" action="task_display.php">
          
             <div class="form-group">
                 <input type="text" class="form-control" id="tasks" placeholder="TODO" name="tasks" required>
             </div>
-            <div class="form-group">
-                <input type="date" class="form-control" id="deadline" placeholder="Deadline" name="deadline" required>
-            </div>
+            <div class="form-group ">
+                <div class="input-group date" id="datetimepicker1">
+                
+                    <input type="text" class="form-control" placeholder="Deadline" name="deadline" required />
+                    <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar" ></span>
+                    </span>
+                </div>
+                </div>
             <button type="submit" class="btn btn-primary btn-lg" name="submit">Submit</button>
             <button type="reset"  class="btn btn-warning btn-lg btn pull-right" name="clear">Clear</button>
         </form>
             </div>
-            </div>
+           <script type="text/javascript">
+            $(function () {
+                $('#datetimepicker1').datetimepicker();
+            });
+        </script> 
+        </div>
         </div>    
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
                 <!-- Latest compiled and minified JavaScript -->
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <!-- Bootstrap Date-Picker Plugin -->
+
+   <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.js"></script>
+			<script src="//cdn.rawgit.com/Eonasdan/bootstrap-datetimepicker/e8bddc60e73c1ec2475f827be36e1957af72e2ea/src/js/bootstrap-datetimepicker.js"></script>
     </body>
 </html>
